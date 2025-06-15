1510fe456573b6eb644e608f5214e0f25726c4be
DonWolfDen <don.rick.wolf@proton.me>
2025-06-15 11:14:03 -0400

    Add Vaultwarden Compose

--- Scripts/Linux/Debian/new_lxc_portainer_agent.sh
+++ Scripts/Linux/Debian/new_lxc_portainer_agent.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Create a Debian 12 Proxmox container with static IP
-# Run this script
+# Run this script with wget -qO- https://raw.githubusercontent.com/DonWolfDen/HomeLab/refs/heads/main/Scripts/Linux/Debian/new_lxc_portainer_agent.sh | bash
 # Open the main portainer gui and go to settings > environments > add > docker standalone > agent
 # The docker command is included. Just enter a name and the static IP:9001
 apt update
